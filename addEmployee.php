ced2eb4c57dae5c6dc76a354ed78988338e9b4a9
Malassri101 <malassri97@gmail.com>
2019-08-29 00:02:53 +0530

    AddEmployee Updated

--- addEmployee.php
+++ addEmployee.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php include('connection.php'); ?>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -34,14 +34,14 @@
                     <div class="col">
                         <!-- First name -->
                         <div class="md-form">
-                            <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                            <input name="fname" type="text" id="materialRegisterFormFirstName" class="form-control">
                             <label for="materialRegisterFormFirstName">First name</label>
                         </div>
                     </div>
                     <div class="col">
                         <!-- Last name -->
                         <div class="md-form">
-                            <input type="text" id="materialRegisterFormLastName" class="form-control">
+                            <input name="lname" type="text" id="materialRegisterFormLastName" class="form-control">
                             <label for="materialRegisterFormLastName">Last name</label>
                         </div>
                     </div>
@@ -49,36 +49,36 @@
 
                  <!-- Username -->
                  <div class="md-form">
-                    <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                    <input name="uname" type="text" id="materialRegisterFormFirstName" class="form-control">
                     <label for="materialRegisterFormFirstName">Username</label>
                 </div>
     
                 <!-- E-mail -->
                 <div class="md-form mt-0">
-                    <input type="email" id="materialRegisterFormEmail" class="form-control">
+                    <input name="email" type="email" id="materialRegisterFormEmail" class="form-control">
                     <label for="materialRegisterFormEmail">E-mail</label>
                 </div>
 
                  <!-- NIC Number -->
                  <div class="md-form mt-0">
-                    <input type="text" id="materialRegisterFormEmail" class="form-control">
+                    <input name="nicnum" type="text" id="materialRegisterFormEmail" class="form-control">
                     <label for="materialRegisterFormEmail">NIC Number</label>
                 </div>
 
                  <!-- Address -->
                  <div class="md-form mt-0">
-                    <input type="email" id="materialRegisterFormEmail" class="form-control">
+                    <input name="address" type="email" id="materialRegisterFormEmail" class="form-control">
                     <label for="materialRegisterFormEmail">Address</label>
                 </div>
 
                 <div class="md-form mt-0">
-                    <input placeholder="Selected date" type="text" id="date-picker-example" class="form-control datepicker">
+                    <input name="time" placeholder="Selected date" type="text" id="date-picker-example" class="form-control datepicker">
                     <label for="date-picker-example"></label>
                   </div>
     
                 <!-- Password -->
                 <div class="md-form">
-                    <input type="password" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
+                    <input name="password" type="password" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
                     <label for="materialRegisterFormPassword">Password</label>
                     <small id="materialRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
                         At least 8 characters and 1 digit
@@ -87,7 +87,7 @@
     
                 <!-- Phone number -->
                 <div class="md-form">
-                    <input type="text" id="materialRegisterFormPhone" class="form-control" aria-describedby="materialRegisterFormPhoneHelpBlock">
+                    <input name="pnum" type="text" id="materialRegisterFormPhone" class="form-control" aria-describedby="materialRegisterFormPhoneHelpBlock">
                     <label for="materialRegisterFormPhone">Phone number</label>
                     <small id="materialRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
                         Optional - for two step authentication
@@ -95,7 +95,7 @@
                 </div>
     
                 <!-- Sign up button -->
-                <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">ADD EMPLOYEE</button>
+                <button method="post"  name="signup" class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">ADD EMPLOYEE</button>
     
                 <!-- Terms of service -->
                 <p>By clicking
@@ -104,6 +104,51 @@
     
             </form>
             <!-- Form -->
+ <?php
+
+
+if(isset($_POST['signup'])){
+
+  $fname = $_POST['fname'];
+  $lname = $_POST['lname'];
+  $uname = $_POST['uname'];
+  $email = $_POST['email'];
+  $nicnum = $_POST['nicnum'];
+  $address = $_POST['address'];
+  $time = $_POST['time'];
+  $password = $_POST['password'];
+  $nicnum = $_POST['nicnum'];
+  $pnum = $_POST['pnum'];
+  
+
+  $sql = "INSERT INTO employee (firstName, lastName, userName, mail, NICNumber, address, phoneNumber, password)
+  VALUES ('$fname','$lname','$uname','$email','$nicnum','$address','$pnum','$password')";
+
+  if ($conn->query($sql) === TRUE) {
+      echo "Successfully Inserted";
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+}
+
+if(isset($_POST['outtime'])){
+
+  $sql = "UPDATE attendance SET empID='EI111' WHERE test=10";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record updated successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+}
+?> 
+
+
+
     
         </div>
     
