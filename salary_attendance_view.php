d0fb8ce6ce4b96d18629393f62dbfb266c768fbc
Malassri101 <malassri97@gmail.com>
2019-09-01 15:59:56 +0530

    Salary Final

--- salary_attendance_view.php
+++ salary_attendance_view.php
@@ -5,65 +5,123 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
- 
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="css/style.css" rel="stylesheet">
+  </head>
+<body>
+  <!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark primary-color">
+
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="index.html">NimedcoPharmacy - Salary Managment</a>
+  
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+      aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="basicExampleNav">
+  
+      <!-- Links -->
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="salary_dashboard.php">Dasboard
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="salary_attendance_view.php">Attendance</a>
+          <span class="sr-only">(current)</span>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="salary_payroll.php">Payroll</a>
+        </li>
+      
+  
+      </ul>
+      <!-- Links -->
+  
+      <form class="form-inline my-2 my-lg-0 align-self-stretch">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+    <!-- Collapsible content -->
+  
+  </nav>
+  <!--/.Navbar-->
 </head>
 <body>
 
 <?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '';
-$db = 'adminpanel';
+          $mysqli = new mysqli('localhost', 'root', '', 'adminpanel') or die (mysqli_error($mysqli));
+          $result = $mysqli->query("SELECT * FROM salary_payslip") or die($mysqli->error);
+          //pre_r($result);
+          ?> 
 
-// Create connection
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT date,empID,type,reason,email,cnum FROM employeeleave";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    echo "<table>";
+          <!--
+   
+CREATE TABLE salary_payslip(
     
-    <tr>
-    <th>Date &nbsp &nbsp &nbsp &nbsp</th>
-    <th>Emp ID &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</th>
-    <th>Type &nbsp &nbsp &nbsp</th>
-    <th> Reason </th>
-    <th>Email</th>
-    <th>Contact No</th>
-    </tr>
-
-
-    // output data of each row
-
+payslipId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+empId VARCHAR(30) NOT NULL,
+payrollYear YEAR NOT NULL,
+payrollMonth VARCHAR(30) NOT NULL,
+bonus FLOAT(30) NOT NULL,
+panelties FLOAT(30) NOT NULL,
+tax FLOAT(30) NOT NULL,
+otPayment FLOAT(30) NOT NULL,
+dayPayment FLOAT(30) NOT NULL,
+fullPayment FLOAT(30) NOT NULL,
+payslip_gdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>
+) -->   
+        <!-- Extended default form grid -->
+        <table id="dtBasicExample" class="table table-striped table-bordered" cellspacing="0" width="100%">
+            <thead>
+              <tr>
+                <th class="th-sm">Employee Id
+                </th>
+                <th class="th-sm">payroll Year 
+                </th>
+                <th class="th-sm">payroll Month
+                </th>
+                <th class="th-sm">Bonus
+                </th>
+                <th class="th-sm">Tax
+                </th>
+                <th class="th-sm">Full Payment
+                </th>
+                <th class="th-sm">Edit
+                </th>
+                <th class="th-sm">Delete
+                </th>
+                
+              </tr>
+            </thead>
 
-        <td>".$row["date"]."</td>
-        <td>".$row["empID"]."<td>
-        <td>".$row["type"]."<td>
-        <td>".$row["reason"]."<td>
-        <td>".$row["email"]."<td>
-        <td>".$row["cnum"]."<td> 
-        
-    </tr>";
-    <tr><td>
-    echo ' <button type="submit" name="lrequest" class="btn btn-primary btn-md">REGISTER</button>';
-    <td> </tr>
-    }
+            <?php
+              while ($row = $result->fetch_assoc()):?>
 
-    echo "</table>";
-} else {
-    echo "0 results";
-}
-
-$conn->close();
-?>
+              <tr>
+                  <td><?php echo $row['empId']; ?></td>
+                  <td><?php echo $row['payrollYear']; ?></td>
+                  <td><?php echo $row['payrollMonth']; ?></td>
+                  <td><?php echo $row['bonus']; ?></td>
+                  <td><?php echo $row['tax']; ?></td>
+                  <td><?php echo $row['fullPayment']; ?></td>
+                  <td><a href="#?edit=<?php echo $row['id']; ?>"  class="btn btn-info">Edit</a></td>
+                  <td><a href="#?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a></td>
+              </tr>
+                  <?php endwhile; ?>
+            </table>
 
 
 </body>
